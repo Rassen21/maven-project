9751373367b19271182e464e0825380e97e953db
Rassen21 <simpowkin@gmail.com>
2018-04-22 13:51:27 -0400

    New Changes

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -12,7 +12,9 @@ public class Greeter {
   public Greeter() {
 
   }
+  /** {@inheritDoc} 
 
+  */
   public final String greet(final String someone) {
     return String.format("Hello, %s!", someone);
   }
